af1a2ab1910f13b52a895996c5d28aacd2607464
Abhishek Shukla <4747uwu@gmail.com>
2025-05-03 10:37:12 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      context: 'globalThis',
+      treeshake: true
+    }
+  }
 })
